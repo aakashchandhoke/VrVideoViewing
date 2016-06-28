b00e8e47e7baf2abbedcd607cdf0240c6d2e697c
aakash <aakash.chandhoke24@gmail.com>
2016-06-28 12:11:46 +0530

    first commit

---
+++ app/src/main/java/com/example/iaugmentor/vrvideo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.iaugmentor.vrvideo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),SimpleVrVideoActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
